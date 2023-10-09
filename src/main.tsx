77142d0a0750a4ed84ea93a9ce3eba8e9a1daccb
Mai Thanh Phương <48126823+phuong74200@users.noreply.github.com>
2023-10-09 16:53:05 +0000

    vercel analyst

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 
 import App from "@/app";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Analytics />
+  </>,
+);
