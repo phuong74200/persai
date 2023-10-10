b901685bc62f27aa7e17f9c35dadb5ab852cf1c8
Mai Thanh Phương <48126823+phuong74200@users.noreply.github.com>
2023-10-10 04:51:20 +0000

    react-ga

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
+import { initialize } from "react-ga";
 
 import App from "@/app";
+
+initialize("G-59WCR3E2N7", { debug: true });
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(<App />);
